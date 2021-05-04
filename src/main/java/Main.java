80716b6c14af3dc91b0bfb89f1231eec38ef13ae
kkopka <kkopka35@gmail.com>
2021-05-04 13:34:57 +0400

    create

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("First commit");
+        System.out.println("second commit");
+        System.out.println("third commit");
+        System.out.println("4 commit");
+        System.out.println("Hey");
     }
 }
